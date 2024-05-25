a1de71560cd16cb69f741dd0c485c55b43ab7327
Dipak Roy <dipokroydip3@gmail.com>
2024-05-25 13:28:50 -0700

    hero update

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,10 @@
+import Hero from "./pages/Hero";
 
 
 const Home = () => {
       return (
             <div>
-                  
+                  <Hero></Hero>
             </div>
       );
 };
--- src/home/pages/Hero.jsx
+++ src/home/pages/Hero.jsx
@@ -1,9 +1,18 @@
-
+import heroImg from "../../assets/2023-05-12_-_V34_Girl_-_D_522x.webp"
 
 const Hero = () => {
       return (
             <div>
-                  
+                  <div className="flex justify-between items-center w-11/12 m-auto">
+                        <div>
+                              <h1 className="text-4xl font-semibold">Subscribe and Save 50%</h1>
+                              <p className="text-xl py-5">Cancel anytime, save a massive 50% on V34 when you subscribe!</p>
+                              <button className="bg-black mt-5 text-white px-10 py-2 rounded-full font-semibold">Subscribe & Save</button>
+                        </div>
+                        <div>
+                              <img src={heroImg} alt="" />
+                        </div>
+                  </div>
             </div>
       );
 };
